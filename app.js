44c02673d2d3955dfd78fdbfe02083ee74a95c1b
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-07-03 11:12:24 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,83 @@
+const express = require('express')
+const bodyparser = require('body-parser')
+const fs = require('fs');
+const path = require('path')
+const mysql = require('mysql')
+const multer = require('multer')
+const csv = require('fast-csv');
+ 
+const app = express()
+app.use(express.static("./public"))
+ 
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({
+    extended: true
+}))
+ 
+// Database connection
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "test"
+})
+ 
+db.connect(function (err) {
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log('Connected to database.');
+})
+ 
+var storage = multer.diskStorage({
+    destination: (req, file, callBack) => {
+        callBack(null, './uploads/')    
+    },
+    filename: (req, file, callBack) => {
+        callBack(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+    }
+})
+ 
+var upload = multer({
+    storage: storage
+});
+ 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+ 
+app.post('/import-csv', upload.single("import-csv"), (req, res) =>{
+    uploadCsv(__dirname + '/uploads/' + req.file.filename);
+    console.log('File has imported :' + err);
+});
+ 
+function uploadCsv(uriFile){
+    let stream = fs.createReadStream(uriFile);
+    let csvDataColl = [];
+    let fileStream = csv
+        .parse()
+        .on("data", function (data) {
+            csvDataColl.push(data);
+        })
+        .on("end", function () {
+            csvDataColl.shift();
+  
+            db.connect((error) => {
+                if (error) {
+                    console.error(error);
+                } else {
+                    let query = 'INSERT INTO users (id, name, email) VALUES ?';
+                    db.query(query, [csvDataColl], (error, res) => {
+                        console.log(error || res);
+                    });
+                }
+            });
+             
+            fs.unlinkSync(uriFile)
+        });
+  
+    stream.pipe(fileStream);
+}
+ 
+const PORT = process.env.PORT || 5555
+app.listen(PORT, () => console.log(`Node app serving on port: ${PORT}`));
